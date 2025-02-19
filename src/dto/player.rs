22b383a50c9a4fed0165814887ff5a650cabafc7
frndotmsn <ferthomsencanales@icloud.com>
2025-02-19 20:44:38 +0000

    now using sqlx macros instead of the functions, more dx now through compile time checks

--- src/dto/player.rs
+++ src/dto/player.rs
@@ -3,4 +3,5 @@ pub struct Player {
     pub id: i32,
     pub name: String,
     pub selected_meme_template_id: Option<i32>,
+    pub rerolls_left: i32,
 }
--- src/player_manager.rs
+++ src/player_manager.rs
@@ -13,17 +13,6 @@ pub struct PlayerManager {
 }
 
 async fn select_new_meme_template(database_connection_pool: &PgPool, player_id: i32) -> anyhow::Result<Option<i32>> {
-    // TODO: exclude meme templaces being labelled by other users and the previous one!
-    // selects meme_templates being currently labelled by users
-    // also includes the meme_template being labelled by the current user
-    /* let _ = sqlx::query_scalar("SELECT (id) FROM (meme_templates INNER JOIN players ON players.selected_meme_template = meme_templates.id)")
-        .fetch_all(&pool)
-        .await?; */
-    // old version without excluding meme templates
-    /* let new_meme_template_id: i32 = sqlx::query_scalar("SELECT (id) FROM meme_templates ORDER BY RANDOM() LIMIT 1")
-        .fetch_one(&self.database_connection_pool)
-        .await?; */
-
     // but we also need to exclude meme_templates that were previously seen by the current user!
     // achieve this through a union (ALL???? performance? dunno) in the except clause!
 
@@ -32,13 +21,15 @@ async fn select_new_meme_template(database_connection_pool: &PgPool, player_id: 
     // would panic when using fetch_one when no meme template can be selected
     // INNER JOIN because we need the values to match!
     // we need to use a subquery for the except clause as well
-    // see https://www.sqlite.org/lang_select.html#compound
-    // doesnt work as expected with postgresql!
-    // meme_templates arent omitted when they should be!
-    let new_meme_template_id: Option<i32> = sqlx::query_scalar("
-        SELECT main_query.id
+
+    // we do not need to specify the type of "main_query.id"
+    // just to say it's forced not-null
+    // see https://docs.rs/sqlx/latest/sqlx/macro.query.html#type-overrides-output-columns
+    let new_meme_template_id: Option<i32> = sqlx::query_scalar!(
+        r#"
+        SELECT main_query.id as "id!"
         FROM (
-            SELECT id
+            SELECT meme_templates.id
             FROM meme_templates
             EXCEPT
             SELECT meme_templates_to_omit.id
@@ -54,8 +45,8 @@ async fn select_new_meme_template(database_connection_pool: &PgPool, player_id: 
                 ON meme_templates_seen_by_players.meme_template_id = meme_templates.id
                 WHERE meme_templates_seen_by_players.player_id = $1
             ) AS meme_templates_to_omit
-        ) AS main_query ORDER BY RANDOM() LIMIT 1")
-        .bind(player_id)
+        ) AS main_query
+        ORDER BY RANDOM() LIMIT 1"#, player_id)
         .fetch_optional(database_connection_pool)
         .await?;
     Ok(new_meme_template_id)
@@ -73,8 +64,7 @@ impl PlayerManager {
     }
 
     pub async fn sign_in_as_guest(&self, name: String) -> anyhow::Result<()> {
-        let player_id: i32 = sqlx::query_scalar("INSERT INTO players (name) VALUES ($1) RETURNING id")
-            .bind(name)
+        let player_id: i32 = sqlx::query_scalar!(r#"INSERT INTO players (name) VALUES ($1) RETURNING id as "id!""#, name)
             .fetch_one(&self.database_connection_pool)
             .await?;
 
@@ -91,8 +81,10 @@ impl PlayerManager {
     }
 
     pub async fn get_player_by_id(&self, id: i32) -> anyhow::Result<Player> {
-        let player: Player = sqlx::query_as("SELECT * FROM players WHERE id = $1")
-            .bind(id)
+        let player: Player = sqlx::query_as!(
+            Player,
+            "SELECT * FROM players WHERE id = $1",
+            id)
             .fetch_one(&self.database_connection_pool)
             .await?;
         Ok(player)    
@@ -106,17 +98,19 @@ impl PlayerManager {
     }
 
     pub async fn get_meme_template(&self, meme_template_id: i32) -> anyhow::Result<Option<MemeTemplate>> {
-        let meme_template: Option<MemeTemplate> = sqlx::query_as("SELECT * FROM meme_templates WHERE id = $1")
-            .bind(meme_template_id)
+        let meme_template: Option<MemeTemplate> = sqlx::query_as!(
+                MemeTemplate,
+                "SELECT * FROM meme_templates WHERE id = $1",
+                meme_template_id)
             .fetch_optional(&self.database_connection_pool)
             .await?;
         Ok(meme_template)
     }
 
     pub async fn get_meme_template_id_for_player(&self, user_id: i32) -> anyhow::Result<Option<i32>> {
-        // TODO: make sure different users get different memes through pool manipulation or something
-        let id: Option<i32> = sqlx::query_scalar("SELECT (selected_meme_template_id) FROM players WHERE id = $1")
-            .bind(user_id)
+        let id: Option<i32> = sqlx::query_scalar!(
+            r#"SELECT selected_meme_template_id as "selected_meme_template_id?" FROM players WHERE id = $1"#,
+            user_id)
             // fetch_one instead of fetch_optional, because fetch_optional will always return Some(Option(...)) here and fetch_one does what we want
             // default value is NULL in SQL, but there is always a row present if id is valid!
             .fetch_one(&self.database_connection_pool)
@@ -131,15 +125,18 @@ impl PlayerManager {
         // maybe limit max players depending on memes?
         let Some(new_meme_template_id) = new_meme_template_id else { return Err(anyhow!("couldnt pick a new meme template, as all were seen or in use")) };
 
-        sqlx::query("UPDATE players SET selected_meme_template_id = $1 WHERE id = $2")
-            .bind(new_meme_template_id)
-            .bind(player_id)
+        sqlx::query!("UPDATE players SET selected_meme_template_id = $1 WHERE id = $2",
+            new_meme_template_id,
+            player_id)
             .execute(&self.database_connection_pool)
             .await?;
 
         // always do this after picking a meme instead of before choosing a new one
         // else a player might see a meme twice (but not through rerolling, just rejoining game) without it being registered
-        sqlx::query("INSERT INTO meme_templates_seen_by_players (player_id, meme_template_id) VALUES ($1, $2)")
+        sqlx::query!("INSERT INTO meme_templates_seen_by_players (player_id, meme_template_id) VALUES ($1, $2)",
+            player_id,
+            new_meme_template_id
+            )
             .bind(player_id)
             .bind(new_meme_template_id)
             .execute(&self.database_connection_pool)
@@ -156,22 +153,21 @@ impl PlayerManager {
     }
 
     pub async fn get_remaining_rerolls(&self, player_id: i32) -> anyhow::Result<i32> {
-        let rerolls_left: i32 = sqlx::query_scalar("SELECT (rerolls_left) FROM players WHERE id = $1")
-            .bind(player_id)
+        let rerolls_left: i32 = sqlx::query_scalar!(r#"SELECT rerolls_left as "rerolls_left!" FROM players WHERE id = $1"#, player_id)
             .fetch_one(&self.database_connection_pool)
             .await?;
 
         Ok(rerolls_left)
-    }
+    } 
 
     pub fn can_reroll(&self, no_of_remaining_rerolls: i32) -> bool {
         no_of_remaining_rerolls > 0
     }
 
     pub async fn set_rerolls_left(&self, new_no_of_rerolls: i32, player_id: i32) -> anyhow::Result<()> {
-        sqlx::query("UPDATE players SET rerolls_left = $1 WHERE id = $2")
-            .bind(new_no_of_rerolls)
-            .bind(player_id)
+        sqlx::query!("UPDATE players SET rerolls_left = $1 WHERE id = $2",
+            new_no_of_rerolls,
+            player_id)
             .execute(&self.database_connection_pool)
             .await?;
         Ok(())
